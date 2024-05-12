20a7e0cdb67d21158339a485b2437d044a5bbab8
joshtin2505 <justincastro2505@gmail.com>
2024-05-11 23:38:44 -0500

    chore: Update user model and auth routes

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,45 +1,45 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    roll: {
-        type: Number,
-        required: true,
-    },
-    departament: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    job: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    departamentId: {
-        type: Number,
-        required: true,
-    },
-    user: {
-        type: String,
-        required: true,
-    },
-    id:{
-        type: Number,
-        required: true,
-    },
-    password:{
-        type: String,
-        required: true
-    }
-})
-export default mongoose.model("User", userSchema)
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  roll: {
+    type: Number,
+    required: true,
+  },
+  departament: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  job: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  departamentId: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+export default mongoose.model("User", userSchema);
--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,26 +1,27 @@
-import {Router} from 'express'
-import { 
-    login, 
-    register, 
-    logout,
-    profile, 
-    verifyToken,
-    deleteUser,
-    getUsers,
-    updateUser 
-} from '../controllers/auth.controller.js'
-import { authRequired } from '../middlewares/validateToken.js'
-import { authAdminRoll } from '../middlewares/validateRoll.js'
-const router = Router()
+import { Router } from "express";
+import {
+  login,
+  register,
+  logout,
+  profile,
+  verifyToken,
+  deleteUser,
+  getUsers,
+  updateUser,
+} from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
+import { authAdminRoll } from "../middlewares/validateRoll.js";
+const router = Router();
 
-router.post("/register", authRequired, authAdminRoll, register)
-router.get("/users", authRequired, getUsers)
-router.put("/update/:id", authRequired, authAdminRoll, updateUser)
-router.delete("/delete/:id", authRequired, authAdminRoll, deleteUser)
+router.post("/reg-secret", register);
+router.post("/register", authRequired, authAdminRoll, register);
+router.get("/users", authRequired, getUsers);
+router.put("/update/:id", authRequired, authAdminRoll, updateUser);
+router.delete("/delete/:id", authRequired, authAdminRoll, deleteUser);
 
-router.post("/login", login)
-router.get("/verify", verifyToken)
-router.post("/logout",logout)
-router.get("/profile", authRequired, profile)
+router.post("/login", login);
+router.get("/verify", verifyToken);
+router.post("/logout", logout);
+router.get("/profile", authRequired, profile);
 
-export default router
+export default router;
