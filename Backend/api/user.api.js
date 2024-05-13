ba6ae8c4f978732912bdc2b56ab078b379b327bf
joshtin2505 <justincastro2505@gmail.com>
2024-05-13 13:00:49 -0500

    feat: Refactor getUserByUserName API function and update user.api.js

--- Backend/api/user.api.js
+++ Backend/api/user.api.js
@@ -1,11 +1,10 @@
 import axios from "./axios.js"
 
-const API_URL = "http://localhost:8080/api"
-
 const registerUsuario = (user) => axios.post("/usuario/guardar", user)
 const getUser = (id) => axios.get("usuario/" + id)
-const getUserByUserName = (username) =>
-  axios.get("usuario/username?username=" + username)
+const getUserByUserName = (username) => {
+  return axios.get("usuario/username/" + username)
+}
 const getAllUsers = () => axios.get("/usuario/all")
 const updateUsuario = (id, user) => axios.put("usuario/" + id, user)
 const deleteUsuario = (id) => axios.delete("usuario/" + id)
