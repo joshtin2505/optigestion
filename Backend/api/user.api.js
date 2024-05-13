33937865365feb17c57f74a4550c82215e75b865
joshtin2505 <justincastro2505@gmail.com>
2024-05-12 23:53:44 -0500

    feat: Add getUserByUserName API function

--- Backend/api/user.api.js
+++ Backend/api/user.api.js
@@ -1,11 +1,22 @@
 import axios from "./axios.js"
 
+const API_URL = "http://localhost:8080/api"
+
 const registerUsuario = (user) => axios.post("/usuario/guardar", user)
 const getUser = (id) => axios.get("usuario/" + id)
+const getUserByUserName = (username) =>
+  axios.get("usuario/username?username=" + username)
 const getAllUsers = () => axios.get("/usuario/all")
 const updateUsuario = (id, user) => axios.put("usuario/" + id, user)
 const deleteUsuario = (id) => axios.delete("usuario/" + id)
 
 //  const profile = () => axios.get("usuario/profile") // Para Implementar en la ruta
 
-export { registerUsuario, getAllUsers, updateUsuario, deleteUsuario, getUser }
+export {
+  registerUsuario,
+  getAllUsers,
+  updateUsuario,
+  deleteUsuario,
+  getUser,
+  getUserByUserName,
+}
