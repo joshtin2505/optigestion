152f193f2025dbcba5647b4a42a652df4ae591ff
joshtin2505 <justincastro2505@gmail.com>
2024-05-12 21:04:05 -0500

    Connect db comment

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 import app from "./app.js"
 import { port } from "./config.js"
-import { connectDB } from "./db.js"
+// import { connectDB } from "./db.js"
 
 app.listen(port)
-connectDB()
-console.log(`Server running on port ${port}`)
+// connectDB()
+console.log(`Server running on port http://localhost:${port}`)
