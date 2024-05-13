911895d7e3a67071163c002cb123b3e9d200be7c
joshtin2505 <justincastro2505@gmail.com>
2024-05-12 21:05:10 -0500

    feat: Add user API functions for registration, retrieval, update, and deletion

---
+++ Backend/api/user.api.js
@@ -0,0 +1,11 @@
+import axios from "./axios.js"
+
+const registerUsuario = (user) => axios.post("/usuario/guardar", user)
+const getUser = (id) => axios.get("usuario/" + id)
+const getAllUsers = () => axios.get("/usuario/all")
+const updateUsuario = (id, user) => axios.put("usuario/" + id, user)
+const deleteUsuario = (id) => axios.delete("usuario/" + id)
+
+//  const profile = () => axios.get("usuario/profile") // Para Implementar en la ruta
+
+export { registerUsuario, getAllUsers, updateUsuario, deleteUsuario, getUser }
