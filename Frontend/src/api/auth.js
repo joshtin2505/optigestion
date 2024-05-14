ded807fbe1d23bd017722ce28473640b880f44f4
joshtin2505 <justincastro2505@gmail.com>
2024-05-14 08:54:39 -0500

    refactor: Update auth.js API functions for better organization and readability

--- Frontend/src/api/auth.js
+++ Frontend/src/api/auth.js
@@ -1,16 +1,14 @@
-import axios from './axios.js'
+import axios from "./axios.js"
 
+export const register = (user) => axios.post("/auth/", user)
+export const users = () => axios.get("/auth/")
+export const update = (id, user) => axios.put("/auth/" + id, user)
+export const deleteUser = (id) => axios.delete("/auth/" + id)
 
+export const login = (user) => axios.post("/auth/login", user)
 
-export const register = (user) => axios.post('/register', user)
-export const users = () => axios.get('/users')
-export const update = (id, user) => axios.put('/update/' + id, user)
-export const deleteUser = (id) => axios.delete('/delete/' + id)
+export const verifyToken = () => axios.get("/auth/verify")
 
-export const login = (user)=> axios.post('/login', user)
+export const logout = () => axios.post("/auth/logout")
 
-export const verifyToken = () => axios.get('/verify')
-
-export const logout = () => axios.post('/logout')
-
-export const profile = () => axios.get('/profile')
+export const profile = () => axios.get("/auth/profile")
