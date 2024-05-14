178f61938ed18fa6bc848d765eb59624c4d85463
joshtin2505 <justincastro2505@gmail.com>
2024-05-14 16:10:13 -0500

    refactor: Add routes for roles and departament APIs

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import morgan from "morgan"
 import authRoutes from "./routes/auth.routes.js"
 import requirementsRoutes from "./routes/request.routes.js"
+import rolesRoutes from "./routes/roles.routes.js"
+import departamentRoutes from "./routes/departament.routes.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
@@ -21,5 +23,7 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/req", requirementsRoutes)
+app.use("/api/roles", rolesRoutes)
+app.use("/api/departament", departamentRoutes)
 
 export default app
