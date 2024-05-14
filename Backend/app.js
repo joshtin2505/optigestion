77b480ddd3d7c1424026b52b3d0344afc2fe2baf
joshtin2505 <justincastro2505@gmail.com>
2024-05-14 08:54:05 -0500

    refactor: Update auth.routes.js to use correct endpoint for authentication

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ app.use(morgan("dev"))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/user", authRoutes)
+app.use("/api/auth", authRoutes)
 app.use("/api/req", requirementsRoutes)
 
 export default app
