c8a6e17214b7b57d9b9e2e5bf862d029892c3954
joshtin2505 <justincastro2505@gmail.com>
2024-05-14 09:59:41 -0500

    refactor: Update Profile.jsx to use the new useAuth hook and improve code organization

--- Frontend/src/pages/all users/Profile.jsx
+++ Frontend/src/pages/all users/Profile.jsx
@@ -1,6 +1,6 @@
 import Nav from '../../components/Nav.jsx'
 import '../../assets/css/Profile.css'
-import { useAuth } from '../../context/AuthContext.jsx'
+import useAuth from '../../hooks/useAuth.js'
 import { useEffect } from 'react'
 function Profile() {
 
@@ -9,17 +9,17 @@ function Profile() {
   useEffect(() => {
     getProfile()
   },[])
-  console.log(response)
+  const {nombre, apellido, trabajo, email, username} = response.user
   return (
     <div className='profile-real-cont'>
       <Nav type={1}/>
       <section className='profile'>
         <h1>Perfil</h1>
-        <p className='profile-p'>Nombre: <span className='names'>{response.firstName}</span></p>
-        <p className='profile-p'>Apellidos: <span className='names'>{response.lastName}</span></p>
-        <p className='profile-p'>id: <span className=''>{response.id}</span></p>
-        <p className='profile-p'>User: <span className=''>{response.user}</span></p>
-        <p className='profile-p'>Empleo/Puesto: <span className=''>{response.job}</span></p>
+        <p className='profile-p'>Nombre: <span className='names'>{nombre}</span></p>
+        <p className='profile-p'>Apellidos: <span className='names'>{apellido}</span></p>
+        <p className='profile-p'>Email: <span className=''>{email}</span></p>
+        <p className='profile-p'>User: <span className=''>{username}</span></p>
+        <p className='profile-p'>Empleo/Puesto: <span className=''>{trabajo}</span></p>
       </section>
     </div>
   )
