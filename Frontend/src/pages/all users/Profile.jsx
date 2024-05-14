2c6738bfb92dcb2615157ca963ee693391bd5949
joshtin2505 <justincastro2505@gmail.com>
2024-05-14 15:24:32 -0500

    refactor: Update Profile.jsx to use the new useAuth hook and improve code organization

--- Frontend/src/pages/all users/Profile.jsx
+++ Frontend/src/pages/all users/Profile.jsx
@@ -1,26 +1,46 @@
-import Nav from '../../components/Nav.jsx'
-import '../../assets/css/Profile.css'
-import useAuth from '../../hooks/useAuth.js'
-import { useEffect } from 'react'
+import Nav from "../../components/Nav.jsx"
+import "../../assets/css/Profile.css"
+import useAuth from "../../hooks/useAuth.js"
+import { useEffect, useState } from "react"
 function Profile() {
-
-  const {getProfile,response} = useAuth()
+  const { getProfile} = useAuth()
+  const [response, setResponse] = useState({})
 
   useEffect(() => {
-    getProfile()
-  },[])
-  const {nombre, apellido, trabajo, email, username} = response.user
+
+
+    async function fetchProfile () {
+      getProfile().then((res) => setResponse(res.data)).catch((err) => console.log(err))
+    }
+    fetchProfile()
+  }, [])
+  const { nombre, apellido, trabajo, email, username, departamento } = response
   return (
-    <div className='profile-real-cont'>
-      <Nav type={1}/>
-      <section className='profile'>
-        <h1>Perfil</h1>
-        <p className='profile-p'>Nombre: <span className='names'>{nombre}</span></p>
-        <p className='profile-p'>Apellidos: <span className='names'>{apellido}</span></p>
-        <p className='profile-p'>Email: <span className=''>{email}</span></p>
-        <p className='profile-p'>User: <span className=''>{username}</span></p>
-        <p className='profile-p'>Empleo/Puesto: <span className=''>{trabajo}</span></p>
-      </section>
+    <div className="profile-real-cont">
+      <Nav type={1} />
+      {response && (
+        <section className="profile">
+          <h1>Perfil</h1>
+          <p className="profile-p">
+            Nombre: <span className="names">{nombre ?? "Loading..."}</span>
+          </p>
+          <p className="profile-p">
+            Apellidos: <span className="names">{apellido ?? "Loading..."}</span>
+          </p>
+          <p className="profile-p">
+            Email: <span className="">{email ?? "Loading..."}</span>
+          </p>
+          <p className="profile-p">
+            User: <span className="">{username ?? "Loading..."}</span>
+          </p>
+          <p className="profile-p">
+            Departamento: <span className="">{departamento ?? "Loading..."}</span>
+          </p>
+          <p className="profile-p">
+            Empleo/Puesto: <span className="">{trabajo ?? "Loading..."}</span>
+          </p>
+        </section>
+      )}
     </div>
   )
 }
