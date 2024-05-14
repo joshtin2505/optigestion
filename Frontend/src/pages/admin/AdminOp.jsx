199dcfc0bcb823d5a2dace9cd1d1b0ee4ad4c42b
joshtin2505 <justincastro2505@gmail.com>
2024-05-14 09:59:09 -0500

    To fix

--- Frontend/src/pages/admin/AdminOp.jsx
+++ Frontend/src/pages/admin/AdminOp.jsx
@@ -1,9 +1,15 @@
-import { CreateUserForm, UpdateUserForm } from '../../components/Forms.jsx'
-import '../../assets/css/CreateUser.css'
-import '../../assets/css/ViewUsers.css'
-import { BsArrowClockwise, BsArrowDownSquare, BsArrowUpSquare, BsPencilSquare, BsTrash } from 'react-icons/bs'
-import { useEffect, useState } from 'react'
-import useAuth from '../../hooks/useAuth.js'
+import { CreateUserForm, UpdateUserForm } from "../../components/Forms.jsx"
+import "../../assets/css/CreateUser.css"
+import "../../assets/css/ViewUsers.css"
+import {
+  BsArrowClockwise,
+  BsArrowDownSquare,
+  BsArrowUpSquare,
+  BsPencilSquare,
+  BsTrash,
+} from "react-icons/bs"
+import { useEffect, useState } from "react"
+import useAuth from "../../hooks/useAuth.js"
 import {
   Modal,
   ModalOverlay,
@@ -11,14 +17,14 @@ import {
   ModalHeader,
   ModalBody,
   ModalCloseButton,
-  useDisclosure
-} from '@chakra-ui/react'
+  useDisclosure,
+} from "@chakra-ui/react"
 // Renderiza todo el dashboard de el Administrador
 function AdminOp() {
   return (
     <>
-      <CreateUser/>
-      <Users/>
+      <CreateUser />
+      <Users />
     </>
   )
 }
@@ -26,21 +32,35 @@ function AdminOp() {
 function CreateUser() {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className='reg-true-cont'>
-      <section className='reg-container'>
+    <div className="reg-true-cont">
+      <section className="reg-container">
         <h1>Register</h1>
         {/* abrir el form registro de usuarios */}
-        {
-          isOpen ? <BsArrowUpSquare className='open-reg' size={25} fill='#2b2b2b' cursor='pointer' onClick={() => setIsOpen(false)}/> : <BsArrowDownSquare className='open-reg' size={25} fill='#2b2b2b' cursor='pointer' onClick={() => setIsOpen(true)}/>
-        }
-        <div className={`body ${isOpen ? 'show': 'hidden'}`}>
-          <CreateUserForm/>
+        {isOpen ? (
+          <BsArrowUpSquare
+            className="open-reg"
+            size={25}
+            fill="#2b2b2b"
+            cursor="pointer"
+            onClick={() => setIsOpen(false)}
+          />
+        ) : (
+          <BsArrowDownSquare
+            className="open-reg"
+            size={25}
+            fill="#2b2b2b"
+            cursor="pointer"
+            onClick={() => setIsOpen(true)}
+          />
+        )}
+        <div className={`body ${isOpen ? "show" : "hidden"}`}>
+          <CreateUserForm />
           <aside>
             <h3>Roll</h3>
-            <p className='roles'>0 = Admin</p>
-            <p className='roles'> 1 = Rector</p>
-            <p className='roles'> 2 = logistico</p>
-            <p className='roles'> 3 = Operativo</p>
+            <p className="roles">0 = Admin</p>
+            <p className="roles"> 1 = Rector</p>
+            <p className="roles"> 2 = logistico</p>
+            <p className="roles"> 3 = Operativo</p>
           </aside>
         </div>
       </section>
@@ -48,12 +68,11 @@ function CreateUser() {
   )
 }
 // Muestra usuarios
-function Users (){
-  
-  const {getUsers, newRender,setNewRender} = useAuth()
+function Users() {
+  const { getUsers, newRender, setNewRender } = useAuth()
   const [users, setUsers] = useState([])
   const [refresh, setRefresh] = useState(false)
-  useEffect(()=>{
+  useEffect(() => {
     const fetchUsers = async () => {
       try {
         const res = await getUsers()
@@ -63,75 +82,77 @@ function Users (){
       }
     }
     if (newRender === true || refresh === true) {
-      setTimeout(()=>{
+      setTimeout(() => {
         setNewRender(false)
         setRefresh(false)
         fetchUsers()
-      },300)
+      }, 300)
     }
-    
-  },[newRender,refresh])
-  
-  
-  return(
+  }, [newRender, refresh])
+
+  return (
     <>
-      <section className='users-container'>
+      <section className="users-container">
         <div className="userCont">
-          <section className='users-head'>
+          <section className="users-head">
             <h1>Users</h1>
-            <button onClick={()=> setRefresh(true)} className='user-refresh-btn'>
+            <button
+              onClick={() => setRefresh(true)}
+              className="user-refresh-btn"
+            >
               <span>Refresh</span>
-              <BsArrowClockwise size={20} fill='#6b6b6b'/>
+              <BsArrowClockwise size={20} fill="#6b6b6b" />
             </button>
           </section>
-          <ul className='users-card-container'>
-            {
-              users.map((user,index) => {
-                const fullName = `${user.firstName} ${user.lastName}`
-                const roll = user.roll === 0 ? 'Admin' : user.roll === 1 ? 'Rector' : user.roll === 2 ? 'Logistico' : user.roll === 3 ? 'Operativo' : 'No Role'
-                
-                return <List key={user._id} fullName={fullName} roll={roll} user={user} index={index} />
-                
-              })
-          }
+          <ul className="users-card-container">
+            {users.map((user, index) => {
+              return <List key={index} user={user} index={index} />
+            })}
           </ul>
         </div>
       </section>
     </>
   )
 }
-const List = ({user, roll, fullName, index}) =>{
-  const {userDelete, setNewRender} = useAuth()
-  const [openUpdateForm, setOpenUpdateForm] = useState('hide')
+const List = ({ user, index }) => {
+  const { userDelete, setNewRender } = useAuth()
+  const [openUpdateForm, setOpenUpdateForm] = useState("hide")
   const { isOpen, onOpen, onClose } = useDisclosure()
-  return(
-    <>
-      <li 
-      className={`user-card uc-${index}`}
-      >
-        <section className='user-infoCont'>
-          <span className='user-info user-id'>{user.id}</span>
-          <span className='user-div'>|</span>
-          <span className='user-info user-name'>{fullName}</span>
-          <span className='user-div'>|</span>
-          <span className='user-info user-job'>{user.job}</span>
-          <span className='user-div'>|</span>
-          <span className='user-info user-nick'>{user.user}</span>
-          <span className='user-div'>|</span>
-          <span className='user-info user-departament'>{user.departament}</span>
-          <span className='user-div'>-</span>
-          <span className='user-info user-departament'>{user.departamentId}</span>
-          <span className='user-div'>|</span>
-          <span className='user-info user-departament'>{roll}</span>
-        </section>
-        <section className='admin-op-container'>
-          <div className="admin-op" onClick={
-            ()=> {
-              setOpenUpdateForm(openUpdateForm === '' ? 'hide' : '')
-            }
-          }><BsPencilSquare/></div>
-          <div className="admin-op" onClick={() => onOpen()}><BsTrash/></div>
 
+  const { id_Usuario, nombre, apellido, rolUsuario } = user
+  const {rol} = rolUsuario
+  console.log(user)
+  const tituloDepartamento = user.departamento.titulo 
+  console.log(tituloDepartamento)
+  return (
+    <>
+      <li className={`user-card uc-${index}`}>
+        <section className="user-infoCont">
+          <span className="user-info user-id">{id_Usuario}</span>
+          <span className="user-div">|</span>
+          <span className="user-info user-name">{`${nombre} ${apellido}`}</span>
+          <span className="user-div">|</span>
+          <span className="user-info user-job">{user.trabajo}</span>
+          <span className="user-div">|</span>
+          <span className="user-info user-nick">{user.username}</span>
+          <span className="user-div">|</span>
+          <span className="user-info user-departament">{tituloDepartamento}</span> {/* TODO: fix this null*/}
+          <span className="user-div">-</span>
+          <span className="user-div">|</span>
+          <span className="user-info user-departament">{rol ? 'no null' : 'null'}</span>{/* TODO: fix this null*/}
+        </section>
+        <section className="admin-op-container">
+          <div
+            className="admin-op"
+            onClick={() => {
+              setOpenUpdateForm(openUpdateForm === "" ? "hide" : "")
+            }}
+          >
+            <BsPencilSquare />
+          </div>
+          <div className="admin-op" onClick={() => onOpen()}>
+            <BsTrash />
+          </div>
         </section>
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
@@ -140,13 +161,20 @@ const List = ({user, roll, fullName, index}) =>{
             <ModalCloseButton />
             <ModalBody>
               <p>¿Estas seguro de eliminar el usuario?</p>
-              <p>{fullName}</p>
-              <button className='AO-delBtn AO-btn-cansel' onClick={onClose}>Cancelar</button>
-              <button className='AO-delBtn AO-btn-del' onClick={()=>{
-                userDelete(user._id)
-                setNewRender(oldRender => !oldRender)
-                onClose()
-              }}>Eliminar</button>
+              <p>{`${nombre} ${apellido}`}</p>
+              <button className="AO-delBtn AO-btn-cansel" onClick={onClose}>
+                Cancelar
+              </button>
+              <button
+                className="AO-delBtn AO-btn-del"
+                onClick={() => {
+                  userDelete(user.id_Usuario)
+                  setNewRender((oldRender) => !oldRender)
+                  onClose()
+                }}
+              >
+                Eliminar
+              </button>
             </ModalBody>
           </ModalContent>
         </Modal>
@@ -154,10 +182,9 @@ const List = ({user, roll, fullName, index}) =>{
       <section className={"updateCont " + openUpdateForm}>
         <div className="updateUserCont">
           <h3>Update User</h3>
-          <UpdateUserForm idDB={user._id} user={user}/>
+          <UpdateUserForm idDB={user.id_Usuario} user={user} />
         </div>
       </section>
-
     </>
   )
 }
