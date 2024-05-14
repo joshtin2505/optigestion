b7097a53d9dc5f01ba95aa256cf6a27004bd7b07
joshtin2505 <justincastro2505@gmail.com>
2024-05-14 15:24:25 -0500

    refactor: Update Forms.jsx to improve code organization and remove unused code

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,152 +1,138 @@
-import { 
-    createContext, 
-    useState, 
-    useEffect 
-} from "react"
-import { 
-    register,
-    users, 
-    update,
-    deleteUser,
-    login,
-    verifyToken,
-    logout ,
-    profile
-} from '../api/auth.js'
-import Cookies from 'js-cookie'
+import { createContext, useState, useEffect } from "react"
+import {
+  register,
+  users,
+  update,
+  deleteUser,
+  login,
+  verifyToken,
+  logout,
+  profile,
+} from "../api/auth.js"
+import Cookies from "js-cookie"
 export const AuthContext = createContext()
 
+export const AuthContextProvider = ({ children }) => {
+  const [response, setResponse] = useState(null)
+  const [isAutenticated, setIsAuthenticated] = useState(false)
+  const [isAllowed, setIsAllowed] = useState(null)
+  const [errors, setErrors] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [closedSession, setClosedSession] = useState(false)
+  const [newRender, setNewRender] = useState(true)
+  const singUp = async (user) => {
+    try {
+      const res = await register(user)
+      return res
+    } catch (error) {
+      setErrors(error.response.data)
+    }
+  }
+  const getUsers = async () => {
+    try {
+      const res = await users()
+      return res
+    } catch (err) {
+      setErrors(err)
+    }
+  }
+  const updateUser = async (id, user) => {
+    try {
+      const res = await update(id, user)
+      return res
+    } catch (error) {
+      setErrors(error.response.data)
+    }
+  }
+  const userDelete = async (id) => {
+    try {
+      const res = await deleteUser(id)
 
+      return res
+    } catch (err) {
+      setErrors(err.response.data)
+    }
+  }
+  const singIn = async (user) => {
+    try {
+      const res = await login(user)
+      setIsAuthenticated(true)
+      setResponse(res.data)
+    } catch (err) {
+      setErrors(err.response.data)
+    }
+  }
+  const logOut = async () => {
+    try {
+      const res = await logout()
+      if (!res) return console.log("No se pudo cerrar sesión")
+      setClosedSession(!closedSession)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const getProfile = async () => {
+    return await profile()
+  }
+  useEffect(() => {
+    async function checkLogin() {
+      const cookies = Cookies.get()
+      if (!cookies.token) {
+        setIsAuthenticated(false)
+        setLoading(false)
+        return setResponse(null)
+      }
+      try {
+        const res = await verifyToken(cookies.token)
+        if (!res.data) {
+          setIsAuthenticated(false)
+          setLoading(false)
+          return
+        }
+        if (res.data.user.rolUsuario.id_rol === 0) setIsAllowed(0)
+        else if (res.data.user.rolUsuario.id_rol === 1) setIsAllowed(1)
+        else if (res.data.user.rolUsuario.id_rol === 2) setIsAllowed(2)
+        else if (res.data.user.rolUsuario.id_rol === 3) setIsAllowed(3)
+        setIsAuthenticated(true)
+        setResponse(res.data)
+        setLoading(false)
+      } catch (error) {
+        setIsAuthenticated(false)
+        setResponse(null)
+        setLoading(false)
+      }
+    }
+    checkLogin()
+  }, [closedSession])
 
-export const AuthContextProvider = ({children}) => {
-    const [response, setResponse] = useState(null)
-    const [isAutenticated, setIsAuthenticated] = useState(false)
-    const [isAllowed, setIsAllowed] = useState(null)
-    const [errors, setErrors] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [closedSession, setClosedSession] = useState(false)
-    const [newRender, setNewRender] = useState(true)
-    const singUp = async(user) => {
-        try {
-            const res = await register(user)
-            return res
-        } catch (error) {
-            setErrors(error.response.data)
-        }
-        
-
+  useEffect(() => {
+    if (errors.length > 0) {
+      const timer = setTimeout(() => {
+        setErrors([])
+      }, 5000)
+      return () => clearTimeout(timer)
     }
-    const getUsers = async() =>{
-        try {
-            const res = await users()
-            return res
-        } catch (err) {
-           setErrors(err) 
-        }
-    }
-    const updateUser = async(id,user) => {
-        try {
-            const res = await update(id,user)
-            return res            
-        } catch (error) {
-            setErrors(error.response.data)
-        }
-    }
-    const userDelete = async(id) => {
-        try {
-            const res = await deleteUser(id)
-            
-            return res
-        } catch (err) {
-            setErrors(err.response.data)
-        }
-
-    }
-    const singIn = async (user) =>{
-        try {
-            const res = await login(user)
-            setIsAuthenticated(true)
-            setResponse(res.data)
-        } catch (err) {
-            setErrors(err.response.data)
-        }
-    }
-    const logOut = async () =>{
-        try {
-            const res = await logout()
-            if(!res) return console.log("No se pudo cerrar sesión")
-            setClosedSession(!closedSession)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    const getProfile = async () =>{
-        try {
-            const res = await profile()
-            if (!res) throw new Error('Error!!! - No response')
-            return res // aqui hay un fenomeno raro
-        } catch (error) {
-            setErrors(error.response.data)
-        }
-    }
-    useEffect(()=>{
-        async function  checkLogin (){
-            const cookies = Cookies.get()
-            if (!cookies.token){
-                setIsAuthenticated(false)
-                setLoading(false)
-                return setResponse(null)
-            }
-                try {
-                    const res = await verifyToken(cookies.token)
-                    if (!res.data) {
-                        setIsAuthenticated(false)
-                        setLoading(false)
-                        return
-                    }
-                    if(res.data.user.rolUsuario.id_rol === 0 ) setIsAllowed(0)
-                    else if(res.data.user.rolUsuario.id_rol === 1) setIsAllowed(1)
-                    else if(res.data.user.rolUsuario.id_rol === 2) setIsAllowed(2)
-                    else if(res.data.user.rolUsuario.id_rol === 3) setIsAllowed(3)
-                    setIsAuthenticated(true)
-                    setResponse(res.data)
-                    setLoading(false)
-
-                } catch (error) {
-                    setIsAuthenticated(false)
-                    setResponse(null)
-                    setLoading(false)
-                }
-        }
-        checkLogin()
-    },[closedSession])  
-
-    useEffect(() => {
-        if (errors.length > 0 ){
-            const timer = setTimeout(()=> {
-                setErrors([])
-            }, 5000)
-            return () => clearTimeout(timer)
-        }
-    },[errors])
-    return(
-        <AuthContext.Provider value={{
-            singUp,
-            getUsers,
-            updateUser,
-            userDelete,
-            singIn,
-            logOut,
-            getProfile,
-            setNewRender,
-            newRender,
-            response,
-            isAutenticated,
-            isAllowed,
-            errors,
-            loading,
-        }}>
-            {children}
-        </AuthContext.Provider>
-    )
+  }, [errors])
+  return (
+    <AuthContext.Provider
+      value={{
+        singUp,
+        getUsers,
+        updateUser,
+        userDelete,
+        singIn,
+        logOut,
+        getProfile,
+        setNewRender,
+        newRender,
+        response,
+        isAutenticated,
+        isAllowed,
+        errors,
+        loading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
 }
