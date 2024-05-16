21532e6f7ee99a171508af537f5b40bdcf4be39a
joshtin2505 <justincastro2505@gmail.com>
2024-05-16 14:33:24 -0500

    refactor: Update CrearSolicitud component to remove unnecessary code

--- Frontend/src/pages/all users/CrearSolicitud.jsx
+++ Frontend/src/pages/all users/CrearSolicitud.jsx
@@ -3,14 +3,11 @@ import '../../assets/css/CreateReq.css'
 import { CreateReqForm } from '../../components/Forms.jsx'
 
 function CrearSolicitud() {
-  
-
   return (
     <div className='back'>
       <Nav type={1}/>
       <section className='CR-box-cont'>
           <CreateReqForm />
-
       </section>
     </div>
   )
