50592a006c5f0df72217396c615f9473b724c485
joshtin2505 <justincastro2505@gmail.com>
2024-05-16 11:50:13 -0500

    refactor: Update Archivado component to handle response data and search functionality

--- Frontend/src/pages/all users/Archivado.jsx
+++ Frontend/src/pages/all users/Archivado.jsx
@@ -17,7 +17,7 @@ function Archivado() {
   useEffect(() => {
     const fetchReq = async () => {
       const res = await getAllFileReq()
-      setResponse(res)
+      setResponse(res.data)
     }
 
     fetchReq()
@@ -26,7 +26,7 @@ function Archivado() {
     e.preventDefault()
   }
 
-  const filteredResponse = response.filter((req) => {
+  const filteredResponse = response?.filter((req) => {
     return (
       req.title.toLowerCase().includes(search.toLowerCase()) ||
       req.description.toLowerCase().includes(search.toLowerCase())
